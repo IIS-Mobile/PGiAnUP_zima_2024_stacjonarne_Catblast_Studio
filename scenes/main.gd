174047061d43e49f1fa8f01f020da514894223b5
jgrodzki <240675@edu.p.lodz.pl>
2024-10-24 18:58:27 +0200

    gear momentum

---
+++ scenes/main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var counter = get_node("UI/Labels/Label_resource1")
+
+func _ready() -> void:
+	counter.text = "0"
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		counter.text = str(int(counter.text)+1)
+		gear.speed = min(gear.speed + 10, 50)
+		
+func _physics_process(_delta: float) -> void:
+	gear.speed = max(gear.speed * 0.99, 0)
