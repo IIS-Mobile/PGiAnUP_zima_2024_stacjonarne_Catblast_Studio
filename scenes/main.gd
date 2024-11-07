6ae23ee9842db327eb3568b416656fe264d52a1b
Matx1002 <35429241+Matx1002@users.noreply.github.com>
2024-11-08 00:45:56 +0100

    Add test game save

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,11 +1,46 @@
 extends Node2D
 
-@onready var counter = [get_node("UI/Labels/Label_resource1"),get_node("UI/Labels/Label_resource2"),get_node("UI/Labels/Label_resource3"),get_node("UI/Labels/Label_resource4")]
+var counter
+
+func _ready():
+	counter = get_node("UI/VBoxContainer/ColorRect/Resource1/Label_resource2")
+	load_game_data()
 
 func _on_gear_button_pressed() -> void:
 	$UI/VBoxContainer/HSplitContainer/ScrollContainer/GearContainer.add_gear()
 
 func _on_gear_container_rotation_completed(index: Variant) -> void:
 	#TODO: placeholder
-	var counter = counter[index / 4]
 	counter.text = str(int(counter.text) + 1)
+	pass
+
+func save_game_data():
+	print(counter.get_text())
+	var save_data = {
+		"resources": [int(counter.get_text())]
+		# "resources": [int(counter[0].text), int(counter[1].text), int(counter[2].text), int(counter[3].text)],
+		# "gears": []
+	}
+	var file = FileAccess.open("user://savegame.json", FileAccess.WRITE)
+	file.store_string(JSON.stringify(save_data))
+	file.close()
+	print("Game saved!")
+
+func load_game_data():
+	var file = FileAccess.open("user://savegame.json", FileAccess.READ)
+	if file != null:
+		var save_data = JSON.new()
+		var json = JSON.new()
+		save_data = json.parse(file.get_as_text())
+		file.close()
+		print("Loaded data: ", save_data)
+		# for i in range(4):
+			# counter[i].text = str(save_data["resources"][i])
+		# counter.text = str(save_data["resources"][0])
+		print("Game loaded!")
+	else:
+		print("No savegame found!")
+
+func _notification(what):
+	if what == NOTIFICATION_APPLICATION_PAUSED:
+		save_game_data()
