a1f9a9fea497ac602662a116bf4f07b5d5185451
jgrodzki <240675@edu.p.lodz.pl>
2024-11-28 19:50:08 +0100

    gear speed multiplier

--- scenes/main.gd
+++ scenes/main.gd
@@ -8,14 +8,13 @@ func _on_gear_button_pressed() -> void:
 	$UI/VBoxContainer/CurrentView/GearsView/ScrollContainer/GearContainer.add_gear()
 
 func _on_gear_container_rotation_completed(index: int) -> void:
+
 	var resource_name = Global.resource_names[index / Global.TIERS_AMOUNT]
 	var tier = index % Global.TIERS_AMOUNT
 	Global.resources[resource_name][tier] += 1
 
-
 func _on_boost_button_pressed() -> void:
 	Global.buffer += 5 * Engine.physics_ticks_per_second * Global.SLOWDOWN_FACTOR
-
 
 func _on_idle_button_pressed() -> void:
 	if Global.taps_count >= Global.STEAM_LIMIT:
