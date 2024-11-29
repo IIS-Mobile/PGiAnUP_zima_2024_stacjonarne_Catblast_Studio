cd591cc7a3330e1190022d530970edea88f4d85d
Matx1002 <35429241+Matx1002@users.noreply.github.com>
2024-11-29 11:40:41 +0100

    Update save and load game

--- scenes/main.gd
+++ scenes/main.gd
@@ -30,9 +30,15 @@ func _notification(what):
 
 func save_game_data():
 	print("Saving game...")
+
 	var save_data = {
-		"resources": [int(counter[0].text), int(counter[1].text), int(counter[2].text)]
+		"save_unix_time" : Time.get_unix_time_from_system(),
+		"resources" : Global.resources,
+		"current_top_tiers" : Global.current_top_tiers,
+		"current_top_resource" : Global.current_top_resource,
+		"gears_count" : Global.count
 	}
+
 	var file = FileAccess.open("user://savegame.json", FileAccess.WRITE)
 	file.store_string(JSON.stringify(save_data))
 	file.close()
@@ -40,15 +46,32 @@ func save_game_data():
 
 func load_game_data():
 	print("Loading game...")
-	var file = FileAccess.open("user://savegame.json", FileAccess.READ)
-	if file != null:
-		var json = JSON.new()
-		var content = file.get_as_text()
-		file.close()
-		json.parse(content)
-		
-		for i in range(3):
-			counter[i].text = str(json.data.get("resources")[i])
+
+	if not FileAccess.file_exists("user://savegame.json"):
+		print("No savegame found!")
+		return
+
+	var save_file = FileAccess.open("user://savegame.json", FileAccess.READ)
+	
+	var json = JSON.new()
+	var content = save_file.get_as_text()
+	save_file.close()
+	var parse_err = json.parse(content)
+
+	if parse_err != OK:
+		print("Error parsing savegame!")
+		return
+
+	if json.data.has("resources"):
+		Global.resources = json.data.get("resources")
+	if json.data.has("current_top_tiers"):
+		Global.current_top_tiers = json.data.get("current_top_tiers")
+	if json.data.has("current_top_resource"):
+		Global.current_top_resource = json.data.get("current_top_resource")
+	if json.data.has("gears_count"):
+		var gears_count = json.data.get("gears_count")
+		for i in range(gears_count):
+			$UI/VBoxContainer/CurrentView/GearsView/ScrollContainer/GearContainer.add_gear()
 
 		print("Game loaded!")
 	else:
