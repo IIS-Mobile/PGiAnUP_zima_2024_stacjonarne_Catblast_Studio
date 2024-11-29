4489ced68eca0a933c013506d0445bcb37fa0a67
Matx1002 <35429241+Matx1002@users.noreply.github.com>
2024-11-29 09:43:41 +0100

    Add proper resource saving

--- scenes/main.gd
+++ scenes/main.gd
@@ -5,6 +5,7 @@ extends Node2D
 func _ready() -> void:
 	#TODO: is there a better way?
 	Global.connect("rotation_completed", _on_gear_container_rotation_completed)
+	load_game_data()
 	
 func _on_gear_button_pressed() -> void:
 	$UI/VBoxContainer/CurrentView/GearsView/ScrollContainer/GearContainer.add_gear()
@@ -23,3 +24,33 @@ func _on_boost_button_pressed() -> void:
 
 func _on_idle_button_pressed() -> void:
 	Global.idle = not Global.idle
+
+func _notification(what):
+	if what == NOTIFICATION_APPLICATION_PAUSED:
+		save_game_data()
+
+func save_game_data():
+	print("Saving game...")
+	var save_data = {
+		"resources": [int(counter[0].text), int(counter[1].text), int(counter[2].text)]
+	}
+	var file = FileAccess.open("user://savegame.json", FileAccess.WRITE)
+	file.store_string(JSON.stringify(save_data))
+	file.close()
+	print("Game saved!")
+
+func load_game_data():
+	print("Loading game...")
+	var file = FileAccess.open("user://savegame.json", FileAccess.READ)
+	if file != null:
+		var json = JSON.new()
+		var content = file.get_as_text()
+		file.close()
+		json.parse(content)
+		
+		for i in range(3):
+			counter[i].text = str(json.data.get("resources")[i])
+
+		print("Game loaded!")
+	else:
+		print("No savegame found!")
